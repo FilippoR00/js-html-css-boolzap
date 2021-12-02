cec1968bf2f768c2f1019926371c47bc1cb75443
Filippo Rossi <rossifilippo2000@gmail.com>
2021-12-02 13:07:58 +0100

    bonus prima parte

--- js/script.js
+++ js/script.js
@@ -13,17 +13,20 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        arrowBox: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
+                        status: 'sent',
+                        arrowBox: false,
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        arrowBox: false,
                     }
                 ],
             },
@@ -35,17 +38,20 @@ const app = new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        arrowBox: false,
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        arrowBox: false,
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'received'
+                        status: 'received',
+                        arrowBox: false,
                     }
                 ],
             },
@@ -57,17 +63,20 @@ const app = new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        arrowBox: false,
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        arrowBox: false,
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        arrowBox: false,
                     }
                 ],
             },
@@ -79,12 +88,14 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        arrowBox: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        arrowBox: false,
                     }
                 ],
             },
@@ -101,14 +112,16 @@ const app = new Vue({
                 this.contacts[this.pointer].messages.push({
                     date: this.createData(),
                     message: this.tempMsg,
-                    status: 'sent'
+                    status: 'sent',
+                    arrowBox: false,
                 });
                 this.tempMsg = "";
                 setTimeout(() => {
                     this.contacts[this.pointer].messages.push({
                         date: this.createData(),
                         message: 'ok',
-                        status: 'received'
+                        status: 'received',
+                        arrowBox: false,
                     });}, 1000);
             }
         },
@@ -143,6 +156,10 @@ const app = new Vue({
             let temp = Math.floor(Math.random() * max - min + 1) + min;
             if(temp < 10) temp = '0' + temp;
             return temp;
+        },
+        deleteMsg(index){
+            this.contacts[this.pointer].messages.splice(index, 1);
+            this.contacts[this.pointer].messages[index].arrowBox = true;
         }
     },
 })
